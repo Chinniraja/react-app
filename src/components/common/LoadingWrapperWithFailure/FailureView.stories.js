c2f8f63a6094e7576d96b358d9f6c35590476eed
EC2 Default User <ec2-user@ip-172-31-29-191.ap-southeast-1.compute.internal>
2020-05-21 17:00:25 +0000

    assignment

--- src/components/common/LoadingWrapperWithFailure/FailureView.stories.js
+++ src/components/common/LoadingWrapperWithFailure/FailureView.stories.js
@@ -1,16 +1,19 @@
+/*global boolean*/
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs, text, color } from '@storybook/addon-knobs/react'
 
-import '../../../styles/tailwind.css'
+// import '../../../styles/tailwind.css'
 import FailureView from './FailureView'
+import Loader from "../Icons/Loader";
 
 export default {
    component: FailureView,
-   title: 'Common/FailureView'
+   title: 'Common/FailureView',
 }
 
-export const defaultView = () => <FailureView />
+// export const defaultView = () => <FailureView />
+// export const loader = () => <Loader/>
 
 export const withOnRetryAndErrorMessageProp = () => (
    <FailureView
@@ -21,10 +24,21 @@ export const withOnRetryAndErrorMessageProp = () => (
 
 export const knobs = () => (
    <FailureView
+      fill={color("fill","#282823c")}
       errorMessage={text('errorMessage', 'failed message')}
       onRetryClick={action('retry clicked')}
    />
 )
+
+export const loaderFn = () => (
+   <Loader
+      fill={color("fill","green")}
+   />
+)
+
+loaderFn.story = {
+   decorators: [withKnobs]
+}
 
 knobs.story = {
    decorators: [withKnobs]
